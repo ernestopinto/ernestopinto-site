144f2574982c2493c76a037266b91c5bd93c310b
Ernesto <pintomiguelernesto@gmail.com>
2026-01-16 17:53:49 +0000

    home grid

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -196,7 +196,7 @@ const getPreviewText = (html: string) => {
 
       <div v-else>
         <!-- Blog Grid -->
-        <div class="grid! grid-cols-1 sm:grid-cols-2! lg:grid!-cols-3 gap-8">
+        <div class="grid! grid-cols-1 sm:grid-cols-3! lg:grid!-cols-3 gap-8">
           <RouterLink
             v-for="post in blogPosts" 
             :key="post.id" 
